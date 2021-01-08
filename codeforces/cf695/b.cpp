cbfe4de3ff0fa69ee46d041b46b4c7d6cb1f0927
Arjit Kansal <kansal.arjit@gmail.com>
2021-01-08 23:01:13 +0530

    Codeforces round 695 - solved 4/5 problems

---
+++ codeforces/cf695/b.cpp
@@ -0,0 +1,126 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+#define mod ((int)1e9+7)
+#define lim 1000000000000000007
+#define lim1 18446744073709551615  //Unsigned
+#define sq(a) ((a)*(a))
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mms(v,i) memset(v,i,sizeof(v))
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REPN(i,a,b) for (int i = a; i >= b; i--)
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> PL;
+typedef pair<ll,int> PLI;
+typedef pair<int,ll> PIL;
+typedef pair<int,pair<int,int> > pii;
+typedef pair<double,double> pdd;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+ll power(ll a,ll b) {
+   a %= mod;
+   ll res=1;
+   while(b){
+      if(b%2==1) res=(res*a)%mod;
+      a=(a*a)%mod;
+      b>>=1;
+   }
+   return res;
+}
+ll gcdll(ll a,ll b) {
+    if (b==0) return a;
+    return gcdll(b,a%b);
+}
+int gcd(int a,int b) {
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+const int N = (int)3e5+5;
+const int M = (int)3e3+5;
+const int Q = 301;
+const int logN = 19;
+
+int a[N], pref[N], n;
+int sum(int l, int r) {
+  return pref[r] - pref[l-1];
+}
+bool extremePoint(int i) {
+  if(i<2 || i>n-1) {
+    return false;
+  }
+  int A = a[i-1], b = a[i], c = a[i+1];
+  return (b>max(A,c) || b<min(A,c));
+}
+void solve() {
+  // int n;
+  cin >> n;
+  REP(i,1,n) {
+    cin >> a[i];
+  }
+  if(n<3) {
+    cout << 0;
+    return;
+  }
+  REP(i,2,n-1) {
+    pref[i] = pref[i-1];
+    if(extremePoint(i)) {
+      pref[i]++;
+    }
+  }
+  pref[n] = pref[n-1];
+  int ans = min(sum(3,n),sum(1,n-2));
+  REP(i,2,n-1) {
+    int curr = a[i];
+    {
+      a[i] = a[i-1];
+      int temp = sum(1,i-2) + sum(i+2,n) + extremePoint(i-1) + extremePoint(i) + extremePoint(i+1);
+      ans = min(ans, temp);
+    }
+    {
+      a[i] = a[i+1];
+      int temp = sum(1,i-2) + sum(i+2,n) + extremePoint(i-1) + extremePoint(i) + extremePoint(i+1);
+      ans = min(ans, temp);
+    }
+    a[i] = curr;
+  }
+  cout << ans;
+}
+int main() {
+    //freopen("output.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T=1;
+    cin>>T;
+    REP(TC,1,T) {
+        //cout<<"Case #"<<TC<<": ";
+        solve();
+        cout<<"\n";
+    }
+}
---
+++ codeforces/cf695/d.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+#define mod ((int)1e9+7)
+#define lim 1000000000000000007
+#define lim1 18446744073709551615  //Unsigned
+#define sq(a) ((a)*(a))
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mms(v,i) memset(v,i,sizeof(v))
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REPN(i,a,b) for (int i = a; i >= b; i--)
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> PL;
+typedef pair<ll,int> PLI;
+typedef pair<int,ll> PIL;
+typedef pair<int,pair<int,int> > pii;
+typedef pair<double,double> pdd;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+ll power(ll a,ll b) {
+   a %= mod;
+   ll res=1;
+   while(b){
+      if(b%2==1) res=(res*a)%mod;
+      a=(a*a)%mod;
+      b>>=1;
+   }
+   return res;
+}
+ll gcdll(ll a,ll b) {
+    if (b==0) return a;
+    return gcdll(b,a%b);
+}
+int gcd(int a,int b) {
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+const int N = (int)2e5+5;
+const int M = (int)3e3+5;
+const int Q = 5005;
+const int logN = 19;
+
+ll a[Q], dp[Q][Q], c[Q];
+void solve() {
+  int n,k,q;
+  cin >> n >> k >> q;
+  REP(i,1,n) {
+    dp[i][0] = 1;
+    cin >> a[i];
+  }
+  REP(j,1,k) {
+    REP(i,1,n) {
+      dp[i][j] = (dp[i-1][j-1] + dp[i+1][j-1]) % mod;
+    }
+  }
+  REP(i,1,n) {
+    REP(j,0,k) {
+      c[i] = (c[i] + dp[i][j] * dp[i][k-j]) % mod;
+    }
+  }
+
+  ll sum = 0;
+  REP(i,1,n) {
+    sum = (sum + a[i] * c[i]) % mod;
+  }
+  int i; ll x;
+  while(q--) {
+    cin >> i >> x;
+    sum = (sum - a[i] * c[i]) % mod;
+    a[i] = x;
+    sum = (sum + a[i] * c[i]) % mod;
+    if(sum < 0) {
+      sum += mod;
+    }
+    cout << sum << "\n";
+  }
+}
+int main() {
+    //freopen("output.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T=1;
+    // cin>>T;
+    REP(TC,1,T) {
+        //cout<<"Case #"<<TC<<": ";
+        solve();
+        // cout<<"\n";
+    }
+}
